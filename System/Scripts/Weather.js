6d18806a7f90affab2665b3618114d0be1e8ef5d
Lewis Britton <lewisbritton23@yahoo.co.uk>
2022-04-28 15:17:08 +0100

    ..

--- System/Scripts/Weather.js
+++ System/Scripts/Weather.js
@@ -5,7 +5,7 @@ fetch('https://raw.githubusercontent.com/FedeRog1977/Burning/master/System/JSON/
     .then((data) => {
         const hills = data;
         const test = document.getElementById("test");
-        test.innerHTML = hills.landmass[0].munro[0].name;
+        test.innerHTML = hills.landmass[1].munro[0].name;
     })
 
 const app = {
