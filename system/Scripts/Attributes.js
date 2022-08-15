8431f219254311c5dd2218b5fabfb1ca103299d3
Lewis Britton <lewisbritton23@yahoo.co.uk>
2022-08-15 13:21:59 +0100

    ..

--- system/Scripts/Attributes.js
+++
@@ -1,4 +0,0 @@
-/*
- * Define JSON for All Attributes
- */
-let attributes = "https://raw.githubusercontent.com/FedeRog1977/Burning/master/System/JSON/Ability-Equipment.json";
--- system/Scripts/Locations.js
+++
@@ -1,4 +0,0 @@
-/*
- * Define JSON for All Locations
- */
-let locations = "https://raw.githubusercontent.com/FedeRog1977/Burning/master/System/JSON/Hills.json";
